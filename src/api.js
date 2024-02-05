b6247d7df66b20452c1c06abfc0b64c0c79acea4
Stanislav Beliaev <testerbeliaev@gmail.com>
2024-02-05 12:23:08 +0300

    removed endless logics from while func

--- src/api.js
+++ src/api.js
@@ -1,11 +1,14 @@
 export const getPlaylists = (limit, offset) => {
 
+  const LIMIT_PARAM = 'limit';
+  const OFFSET_PARAM = 'offset';
+
   const queryParams = new URLSearchParams({
-    limit: limit,
-    offset: offset
+    [LIMIT_PARAM]: limit,
+    [OFFSET_PARAM]: offset
   });
 
- return  fetch(`https://api.spotify.com/v1/me/playlists?${queryParams}`, {
+  return fetch(`https://api.spotify.com/v1/me/playlists?${queryParams}`, {
     headers: {
       Authorization: "Bearer " + sessionStorage.getItem("accessToken")
     },
@@ -43,7 +46,7 @@ export const reorderTrackInPlaylist = (
   })
     .then((response) => response.json());
 
-    
+
 
 export const getPlaylist = (playlistId) =>
   fetch(`https://api.spotify.com/v1/playlists/${playlistId}`, {
